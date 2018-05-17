5bc8761d7c863a77a744b524820a66f994788348
mastercs <mastercs@mail.de>
2018-05-17 15:57:24 +0200

    Adding more Tiles

--- FootballManager2k18/src/main/java/de/kone1k1/games/FootballManager2k18/map/TileType.java
+++ FootballManager2k18/src/main/java/de/kone1k1/games/FootballManager2k18/map/TileType.java
@@ -2,7 +2,8 @@ package de.kone1k1.games.FootballManager2k18.map;
 
 public enum TileType {
 
-	GRASS, STONE, BUSH, TREE, STADIUM;
+	GRASS, STONE, BUSH, BUILDING_TL, BUILDING_TR, NOTHING1, NOTHING2, NOTHING3, NOTHING4, NOTHING5,
+	TREE, STUMP, ROTTENTREE, BUILDING_BL, BUILDING_BR;
 
 	public static TileType getTileByID(int id) {
 		for (TileType t : TileType.values()) {
